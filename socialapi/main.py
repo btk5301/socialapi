e4c9a2aa0fad1da932e1959b23f1c09a2f661c86
Brian <briankhunchana@gmail.com>
2024-10-14 20:33:27 -0500

    added logging setup

--- socialapi/main.py
+++ socialapi/main.py
@@ -1,12 +1,17 @@
+import logging
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from socialapi.database import database
+from socialapi.logging_conf import configure_logging
 from socialapi.routers.post import router as post_roouter
+
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    configure_logging()
     await database.connect()
     yield
     await database.disconnect()
