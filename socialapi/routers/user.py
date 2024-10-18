2c11b44c042963046e8e30bc10e234458e47cdde
Brian <briankhunchana@gmail.com>
2024-10-17 23:07:04 -0500

    added logic for jwt token and testing

--- socialapi/routers/user.py
+++ socialapi/routers/user.py
@@ -2,7 +2,12 @@ import logging
 from fastapi import APIRouter, HTTPException, status
 
 from socialapi.models.user import UserIn
-from socialapi.security import get_user, get_password_hash
+from socialapi.security import (
+    authenticate_user,
+    create_access_token,
+    get_user,
+    get_password_hash,
+)
 from socialapi.database import database, user_table
 
 logger = logging.getLogger(__name__)
@@ -25,3 +30,10 @@ async def register(user: UserIn):
     await database.execute(query)
 
     return {"detail": "User created."}
+
+
+@router.post("/token")
+async def login(user: UserIn):
+    user = await authenticate_user(user.email, user.password)
+    access_token = create_access_token(user.email)
+    return {"access_token": access_token, "token_type": "bearer"}
--- socialapi/security.py
+++ socialapi/security.py
@@ -1,12 +1,35 @@
+import datetime
 import logging
 
+from fastapi import HTTPException, status
+from jose import jwt
 from passlib.context import CryptContext
 
 from socialapi.database import database, user_table
 
 logger = logging.getLogger(__name__)
 
+SECRET_KEY = "6754e1fcb5f9b69e88d7b14e3f036460e89b54fdab026b547d132ee819d4fc22"
+ALGORITHM = "HS256"
 pwd_context = CryptContext(schemes=["bcrypt"])
+
+credentials_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED, detail="could not validate credentials"
+)  # custom error exception
+
+
+def access_token_expire_minutes() -> int:  # used for testing so mocking is easier
+    return 30
+
+
+def create_access_token(email: str):  # storing email since email is unique
+    logger.debug("Creating access token", extra={"email": email})
+    expire = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(
+        minutes=access_token_expire_minutes()
+    )
+    jwt_data = {"sub": email, "exp": expire}
+    encoded_jwt = jwt.encode(jwt_data, key=SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 def get_password_hash(password: str) -> str:
@@ -26,3 +49,13 @@ async def get_user(email: str):
 
     if result:
         return result
+
+
+async def authenticate_user(email: str, password: str):
+    logger.debug("Authenticating user", extra={"email": email})
+    user = await get_user(email)
+    if not user:
+        raise credentials_exception
+    if not verify_password(password, user.password):
+        raise credentials_exception
+    return user
